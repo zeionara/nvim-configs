ed5c529bc63f686f48d497d841126a3ae75e961e
zeio <zeionara@gmail.com>
2022-12-24 01:05:23 +0300

    Updated settings of some language servers

--- lua/ale.lua
+++ lua/ale.lua
@@ -4,5 +4,9 @@ local g = vim.g
 
 -- vim.cmd("let b:ale_linters = {'cpp': ['clangd']}")
 g.ale_cpp_cc_options = '-std=c++20 -I $HOME/randeer/randeer'
+g.ale_linters_ignore = {
+    r = {'languageserver'}
+}
+g.ale_r_lintr_options = "linters_with_defaults(line_length_linter(200), commented_code_linter(NULL))"
 -- vim.cmd("let g:ale_cpp_cc_options = '-std=c++20 -I $HOME/randeer/randeer'")
 -- vim.cmd("let g:ale_cpp_clangd_options = '-std=c++20 -I $HOME/randeer/randeer'")
