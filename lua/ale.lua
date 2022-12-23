1f39bf2ec4676a2f4c99f7cb92bef2cdd6ebf06e
zeio <zeionara@gmail.com>
2022-12-24 01:08:23 +0300

    Added links to documentation for lintr

--- lua/ale.lua
+++ lua/ale.lua
@@ -7,6 +7,6 @@ g.ale_cpp_cc_options = '-std=c++20 -I $HOME/randeer/randeer'
 g.ale_linters_ignore = {
     r = {'languageserver'}
 }
-g.ale_r_lintr_options = "linters_with_defaults(line_length_linter(200), commented_code_linter(NULL))"
+g.ale_r_lintr_options = "linters_with_defaults(line_length_linter(200), commented_code_linter(NULL))" -- configuration docs: https://lintr.r-lib.org/articles/editors.html
 -- vim.cmd("let g:ale_cpp_cc_options = '-std=c++20 -I $HOME/randeer/randeer'")
 -- vim.cmd("let g:ale_cpp_clangd_options = '-std=c++20 -I $HOME/randeer/randeer'")
