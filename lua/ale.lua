854f367a029a354f25ff662748cfada38b6b4c34
zeio <zeionara@gmail.com>
2023-06-22 17:05:36 +0300

    feat(ignored-linters): updated lists of ignored linters for vue and typescript files

--- lua/ale.lua
+++ lua/ale.lua
@@ -5,7 +5,9 @@ local g = vim.g
 -- vim.cmd("let b:ale_linters = {'cpp': ['clangd']}")
 g.ale_cpp_cc_options = '-std=c++20 -I $HOME/randeer/randeer'
 g.ale_linters_ignore = {
-    r = {'languageserver'}
+    r = {'languageserver'},
+    typescript = {'tsserver'},
+    vue = {'vls'}
 }
 g.ale_r_lintr_options = "linters_with_defaults(line_length_linter(200), commented_code_linter(NULL))" -- configuration docs: https://lintr.r-lib.org/articles/editors.html
 -- vim.cmd("let g:ale_cpp_cc_options = '-std=c++20 -I $HOME/randeer/randeer'")
