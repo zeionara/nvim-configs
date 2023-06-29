2ecfd3b3321f932c47c99ee63134c3560bc8666c
Dmitrii <zeionara@gmail.com>
2023-06-29 17:29:45 +0300

    feat(amar): added configs for amar and swim projects

---
+++ projects/amar.lua
@@ -0,0 +1,6 @@
+-- require('lspconfig').vuels.setup{}
+-- cmd = { "vue-language-server", "--stdio", "--resolveJsonModule" }
+
+-- require('lspconfig').volar.setup{
+--     filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'}
+-- }
