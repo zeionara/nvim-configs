3c67d093517c51efe71410df8496b593ef0afe4e
Dmitrii <zeionara@gmail.com>
2026-01-01 20:24:52 +0300

    feat(projects): added project configs

---
+++ projects/much.lua
@@ -0,0 +1,33 @@
+-- require('lspconfig').pylsp.setup{
+--   settings = {
+--     pylsp = {
+--       plugins = {
+--         flake8 = { enabled = true },
+--         pyflakes = { enabled = true },
+--         pydocstyle = { enabled = false },
+--         black = { enabled = true },
+--         autopep8 = { enabled = true },
+--         pycodestyle = {
+--           ignore = { 'E251', 'W504' },
+--           maxLineLength = 200
+--         }
+--       }
+--     }
+--   }
+-- }
+
+vim.lsp.config('pylsp', {
+  plugins = {
+    flake8 = { enabled = true },
+    pyflakes = { enabled = true },
+    pydocstyle = { enabled = false },
+    black = { enabled = true },
+    autopep8 = { enabled = true },
+    pycodestyle = {
+      ignore = { 'E251', 'W504' },
+      maxLineLength = 200
+    }
+  }
+})
+
+vim.lsp.enable('pylsp')
