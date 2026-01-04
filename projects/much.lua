939af9f685e16337032cdfa53398453ab2ae63df
Dmitrii <zeionara@gmail.com>
2026-01-04 13:23:11 +0300

    fix(pylsp): updated pylsp configuration for much project

--- projects/much.lua
+++ projects/much.lua
@@ -17,15 +17,19 @@
 -- }
 
 vim.lsp.config('pylsp', {
-  plugins = {
-    flake8 = { enabled = true },
-    pyflakes = { enabled = true },
-    pydocstyle = { enabled = false },
-    black = { enabled = true },
-    autopep8 = { enabled = true },
-    pycodestyle = {
-      ignore = { 'E251', 'W504' },
-      maxLineLength = 200
+  settings = {
+    pylsp = {
+      plugins = {
+        flake8 = { enabled = true },
+        pyflakes = { enabled = true },
+        pydocstyle = { enabled = false },
+        black = { enabled = true },
+        autopep8 = { enabled = true },
+        pycodestyle = {
+          ignore = { 'E251', 'W504' },
+          maxLineLength = 200
+        }
+      }
     }
   }
 })
